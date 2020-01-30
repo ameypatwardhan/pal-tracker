bd16d92c41f7d11a6aad2f56a2ae1a121043ac2d
training <ameypatwardhan>
2020-01-30 14:48:21 -0800

    health

--- src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
@@ -21,10 +21,7 @@ public class PalTrackerApplication {
         SpringApplication.run(PalTrackerApplication.class, args);
     }
 
-    //@Bean
-    //TimeEntryRepository timeEntryRepository() {
-        //return new InMemoryTimeEntryRepository();
-    //}
+
 
     @Bean
     TimeEntryRepository timeEntryRepository(DataSource dataSource) {
